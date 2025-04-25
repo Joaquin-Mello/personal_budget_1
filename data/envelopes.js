5545ad517b98b7eccf831a063ebf532f974ac51c
Joaquin-Mello <jmellodeveloper@gmail.com>
2025-04-24 21:37:44 -0300

    indentation error

--- data/envelopes.js
+++ data/envelopes.js
@@ -3,48 +3,48 @@ let envelopes = [
     { id: 2, category: 'Rent', balance: 1000 }
   ];
   
-  let nextId = 3;
-  
-  function getAll() {
-    return envelopes;
+let nextId = 3;
+
+function getAll() {
+  return envelopes;
+}
+
+function getById(id) {
+  return envelopes.find(env => env.id === id);
+}
+
+function create(category, balance) {
+  const newEnv = { id: nextId++, category, balance };
+  envelopes.push(newEnv);
+  return newEnv;
+}
+
+function update(id, category, balance) {
+  const envelope = getById(id);
+  if (envelope) {
+    envelope.category = category;
+    envelope.balance = balance;
   }
-  
-  function getById(id) {
-    return envelopes.find(env => env.id === id);
+  return envelope;
+}
+
+function remove(id) {
+  const index = envelopes.findIndex(env => env.id === id);
+  if (index !== -1) {
+    return envelopes.splice(index, 1)[0];
   }
-  
-  function create(category, balance) {
-    const newEnv = { id: nextId++, category, balance };
-    envelopes.push(newEnv);
-    return newEnv;
+  return null;
+}
+
+function transfer(fromId, toId, amount) {
+  const from = getById(fromId);
+  const to = getById(toId);
+  if (from && to && from.balance >= amount) {
+    from.balance -= amount;
+    to.balance += amount;
+    return { from, to };
   }
-  
-  function update(id, category, balance) {
-    const envelope = getById(id);
-    if (envelope) {
-      envelope.category = category;
-      envelope.balance = balance;
-    }
-    return envelope;
-  }
-  
-  function remove(id) {
-    const index = envelopes.findIndex(env => env.id === id);
-    if (index !== -1) {
-      return envelopes.splice(index, 1)[0];
-    }
-    return null;
-  }
-  
-  function transfer(fromId, toId, amount) {
-    const from = getById(fromId);
-    const to = getById(toId);
-    if (from && to && from.balance >= amount) {
-      from.balance -= amount;
-      to.balance += amount;
-      return { from, to };
-    }
-    return null;
-  }
+  return null;
+}
   
 module.exports = { getAll, getById, create, update, remove, transfer };
